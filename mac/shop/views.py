2c2ad9f0339f6d2ec82d1a5588b884e48264a91e
HitarthVaghela <hdvaghela27@gmail.com>
2024-12-30 22:50:17 +0530

    Include Slides

--- mac/shop/views.py
+++ mac/shop/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Product
+from math import ceil
 
 # Create your views here.
 def index(request):
-    return render(request, "shop/index.html")
+    products = Product.objects.all()
+    print(products)
+    n = len(products)
+    nslides = n // 4 + ceil((n/4) - (n//4))
+    params = {'product', products}
+    return render(request, 'shop/index.html', params )
 def about(request):
-    return render(request, "shop/about.html")
+    return render(request, 'shop/about.html')
 def contact(request):
     return HttpResponse("we are at contact")
 def tracker(request):
