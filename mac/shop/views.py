e8814673bafcd0bb4df09595b315b65e4dde58ad
HitarthVaghela <hdvaghela27@gmail.com>
2025-01-01 22:23:02 +0530

    Added Products by Category

--- mac/shop/views.py
+++ mac/shop/views.py
@@ -5,13 +5,19 @@ from math import ceil
 
 # Create your views here.
 def index(request):
-    products = Product.objects.all()
-    print(products)
-    n = len(products)
-    nslides = n // 4 + ceil((n/4) - (n//4))
+    # products = Product.objects.all()
+    # print(products)
+    # n = len(products)
+    # nslides = n // 4 + ceil((n/4) - (n//4))
     # params = {'no_of_slides':nslides,'range':range(1,nslides), 'product': products}
-    allProds = [[products, range(1, nslides) , nslides ],     
-                [products, range(1, nslides) , nslides ]]
+    allProds = []
+    catprods = Product.objects.values('category','id')
+    cat = {item['category'] for item in catprods }
+    for cat in cat:
+        prod = Product.objects.filter(category=cat)
+        n = len(prod)
+        nslides = n // 4 + ceil((n/4) - (n//4))
+        allProds.append([prod, range(1,nslides), nslides])
      
     params = {'allProds': allProds}
     return render(request, 'shop/index.html', params )
