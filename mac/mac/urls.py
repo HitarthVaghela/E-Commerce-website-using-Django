1c90c506cf8a5aeaf1913a1b3eabcb216d2a0c2b
HitarthVaghela <hdvaghela27@gmail.com>
2025-01-04 00:13:39 +0530

    Fixed Error of images and added add to cart

--- mac/mac/urls.py
+++ mac/mac/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     path('shop/', include('shop.urls')),
     path('blog/', include('blog.urls'))
 
-] + static(settings.MEDIA_URL, document_roots= settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
--- mac/shop/urls.py
+++ mac/shop/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path("", views.index, name="ShopHome"),
     path("about/", views.about, name="AboutUs"),
@@ -9,5 +13,9 @@ urlpatterns = [
     path("search/", views.search, name="Search"),
     path("productview/", views.productView, name="ProductView"),
     path("checkout/", views.checkout, name="Checkout"),
-    path("productslist/", views.productlist, name="ProductsItems")
+    path("productslist/", views.productlist, name="ProductsItems"),
 ]
+
+# Serve media files in development mode
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
