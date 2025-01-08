deb346ed1e498e0b22ee159e854b76154732e33a
HitarthVaghela <hdvaghela27@gmail.com>
2025-01-08 21:30:20 +0530

    Added Contact page

--- mac/shop/models.py
+++ mac/shop/models.py
@@ -16,9 +16,9 @@ class Product(models.Model):
 class Contact(models.Model):
     msg_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
-    email = models.CharField(max_length=50 default="")
-    phone = models.CharField(max_length=50 default="")
-    desc = models.CharField(max_length=500 default="")
+    email = models.CharField(max_length=50, default="")
+    phone = models.CharField(max_length=50, default="")
+    desc = models.CharField(max_length=500, default="")
 
 
     def __str__(self):
