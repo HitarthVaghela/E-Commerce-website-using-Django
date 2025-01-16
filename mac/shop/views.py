0496eb7e5964e56300f6951c4140538fe2db59b7
HitarthVaghela <hdvaghela27@gmail.com>
2025-01-16 15:05:42 +0530

    Adding orders

--- mac/shop/views.py
+++ mac/shop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Product, Contact
+from .models import Product, Contact, Orders
 from math import ceil
 
 # Create your views here.
@@ -47,6 +47,17 @@ def productView(request, myid):
     return render(request, 'shop/prodView.html',{'product':product[0]})
 
 def checkout(request):
+    if request.method == "POST":
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        address = request.POST.get('address', '')
+        city = request.POST.get('city', '')
+        state = request.POST.get('state', '')
+        zip_code = request.POST.get('zip_code', '')
+        phone = request.POST.get('phone', '')
+
+        order = Orders(name=name, email=email, address=address, city=city, state=state, zip_code=zip_code, phone=phone)
+        order.save()
     return render(request, 'shop/checkout.html') 
 
 def productlist(request):
