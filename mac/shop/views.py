0388bc45df115307c454792483441fc229650830
HitarthVaghela <hdvaghela27@gmail.com>
2025-01-21 18:34:48 +0530

    Fix Tracker Page

--- mac/shop/views.py
+++ mac/shop/views.py
@@ -22,6 +22,7 @@ def about(request):
     return render(request, 'shop/about.html')
 
 def contact(request):
+    thank = False
     if request.method == "POST":
         name = request.POST.get('name', '')
         email = request.POST.get('email', '')
@@ -29,8 +30,9 @@ def contact(request):
         desc = request.POST.get('desc', '')
         contact = Contact(name=name, email=email, phone=phone, desc=desc)
         contact.save()
+        thank = True
         
-    return render(request, 'shop/contact.html')
+    return render(request, 'shop/contact.html', {'thank' thank})
 
 def tracker(request):
     if request.method == "POST":
