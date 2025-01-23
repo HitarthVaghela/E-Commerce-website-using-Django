5f2b97722f7a9b027516e3307e9986f2ce9e9cf6
HitarthVaghela <hdvaghela27@gmail.com>
2025-01-23 23:17:56 +0530

    Adding Blogpost

--- mac/blog/models.py
+++ mac/blog/models.py
@@ -13,13 +13,5 @@ class Blogpost(models.Model):
     pub_date = models.DateField()
     thumbnail = models.ImageField(upload_to="shop/images", default="")
 
-class Contact(models.Model):
-    msg_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=50)
-    email = models.CharField(max_length=50, default="")
-    phone = models.CharField(max_length=50, default="")
-    desc = models.CharField(max_length=500, default="")
-
-
     def __str__(self):
-        return self.name
+        return self.title
