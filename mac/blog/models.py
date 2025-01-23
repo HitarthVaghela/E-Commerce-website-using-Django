5281134621d6fc1aff4647a21e6d0dcf480de28d
HitarthVaghela <hdvaghela27@gmail.com>
2025-01-23 20:59:35 +0530

    Adding models

--- mac/blog/models.py
+++ mac/blog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Blogpost(models.Model):
+    post_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=50)
+    head0 = models.CharField(max_length=500, default="")
+    head1 = models.CharField(max_length=500, default="")
+    head2 = models.CharField(max_length=500, default="")
+    subcategory = models.CharField(max_length=50, default="")
+    price = models.IntegerField(default=0)
+    desc = models.CharField(max_length=300)
+    pub_date = models.DateField()
+    thumbnail = models.ImageField(upload_to="shop/images", default="")
+
+class Contact(models.Model):
+    msg_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50, default="")
+    phone = models.CharField(max_length=50, default="")
+    desc = models.CharField(max_length=500, default="")
+
+
+    def __str__(self):
+        return self.name
