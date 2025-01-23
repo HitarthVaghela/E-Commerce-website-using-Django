5ee530825fe3af5365157fb6ec4db7dc091aa2ce
HitarthVaghela <hdvaghela27@gmail.com>
2025-01-23 21:03:06 +0530

    Adding models

--- mac/shop/models.py
+++ mac/shop/models.py
@@ -13,6 +13,9 @@ class Product(models.Model):
     pub_date = models.DateField()
     image = models.ImageField(upload_to="shop/images", default="")
 
+    def __str__(self):
+        return self.product_name
+
 class Contact(models.Model):
     msg_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
