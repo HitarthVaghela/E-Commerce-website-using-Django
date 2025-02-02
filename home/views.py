e213d4603f24800fd80d9061f4b0a106b9057c13
HitarthVaghela <hdvaghela27@gmail.com>
2025-02-02 18:19:47 +0530

    Adding Base files

--- home/views.py
+++ home/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Home')
+    return render(request, 'home/home.html')
 
 def about(request):
     return HttpResponse('about')
